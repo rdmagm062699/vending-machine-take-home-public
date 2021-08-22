b9ac4646a288a14f3df2c7a4a2a5a12ba41b7c87
rdmagm062699 <rdmagm062699@gmail.com>
2021-08-22 18:31:45 -0500

    Add starter acceptance tests to get framework set up

---
+++ features/starter.feature
@@ -0,0 +1,6 @@
+Feature: Just getting the framework set up
+
+    Scenario: Do something
+        Given I have something
+        When I do something
+        Then Something happens
---
+++ features/steps/starter.py
@@ -0,0 +1,15 @@
+from behave import *
+
+@given(u'I have something')
+def step_impl(context):
+    pass
+
+
+@when(u'I do something')
+def step_impl(context):
+    pass
+
+
+@then(u'Something happens')
+def step_impl(context):
+    pass
