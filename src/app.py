27cabb9c1200237498c127f4b97d45004fb87b47
rdmagm062699 <rdmagm062699@gmail.com>
2021-08-22 19:54:37 -0500

    Make the vending machine dispense cola

--- src/app.py
+++ src/app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 from flask.templating import render_template
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def vend():
+@app.route('/', methods=['GET'])
+def get_vending_machine():
     return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def post_vending_machine():
+    dispenser = 'DISPENSING COLA...'
+    display = 'THANK YOU'
+
+    return render_template('index.html', dispenser=dispenser, display=display)
