cbf25947004c70bf98ef81544eb72264d81812b6
rdmagm062699 <rdmagm062699@gmail.com>
2021-08-23 17:14:19 -0500

    Modify app to allow for a penny to be added to the machine

--- src/app.py
+++ src/app.py
@@ -5,16 +5,26 @@ from src.machine import Machine
 app = Flask(__name__)
 machine = Machine()
 
+coin_slot = {
+    'PENNY': {'weight': 2.5, 'diameter': .75, 'thickness': 1.52}
+}
 
 @app.route('/', methods=['GET'])
 def get_vending_machine():
     machine = Machine()
-    return render_template('index.html', dispenser=machine.dispenser, display=machine.display, amount_entered='INSERT COIN')
+    coin_return = format(machine.coin_return_in_cents / 100, '.2f')
+    return render_template('index.html', dispenser=machine.dispenser, display=machine.display, amount_entered='INSERT COIN', coin_return=coin_return)
 
 
 @app.route('/', methods=['POST'])
 def post_vending_machine():
-    product = request.form.get('product_button')
-    machine.dispense(product)
+    if request.form.get('product_button'):
+        product = request.form.get('product_button')
+        machine.dispense(product)
 
-    return render_template('index.html', dispenser=machine.dispenser, display=machine.display, amount_entered='INSERT COIN')
+    if request.form.get('coin_button'):
+        dimensions = coin_slot[request.form.get('coin_button')]
+        machine.add_coin(dimensions['weight'], dimensions['diameter'], dimensions['thickness'])
+
+    coin_return = format(machine.coin_return_in_cents / 100, '.2f')
+    return render_template('index.html', dispenser=machine.dispenser, display=machine.display, amount_entered='INSERT COIN', coin_return=coin_return)
