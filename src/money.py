548d6a35e8a35868b60060ecdfa17e3cb342c8a0
rdmagm062699 <rdmagm062699@gmail.com>
2021-08-23 17:36:59 -0500

    WIP - allow for dimes and quarters to be identified by their dimensions

--- src/money.py
+++ src/money.py
@@ -1,7 +1,9 @@
 
 coins = [
     {'weight': 2.5, 'diameter': .75, 'thickness': 1.52, 'value_in_cents': 1, 'valid': False},
-    {'weight': 5.0, 'diameter': .835, 'thickness': 1.95, 'value_in_cents': 5, 'valid': True}
+    {'weight': 5.0, 'diameter': .835, 'thickness': 1.95, 'value_in_cents': 5, 'valid': True},
+    {'weight': 2.268, 'diameter': .705, 'thickness': 1.35, 'value_in_cents': 10, 'valid': True},
+    {'weight': 5.670, 'diameter': .955, 'thickness': 1.75, 'value_in_cents': 25, 'valid': True}
 ]
 
 def identify_coin(weight, diameter, thickness):
--- unit_tests/test_money.py
+++ unit_tests/test_money.py
@@ -12,3 +12,13 @@ class TestMoney(unittest.TestCase):
         coin = identify_coin(5.0, .835, 1.95)
 
         assert coin['value_in_cents'] == 5
+
+    def test_identifies_a_dime(self):
+        coin = identify_coin(2.268, .705, 1.35)
+
+        assert coin['value_in_cents'] == 10
+
+    def test_identifies_a_dime(self):
+        coin = identify_coin(5.670, .955, 1.75)
+
+        assert coin['value_in_cents'] == 25
