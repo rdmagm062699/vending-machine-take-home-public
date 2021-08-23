4fe45edc04b7790957e33c0ab55415b57900b37c
rdmagm062699 <rdmagm062699@gmail.com>
2021-08-23 17:22:33 -0500

    WIP - add logic to identify a nickel by its dimensions

--- src/money.py
+++ src/money.py
@@ -1,7 +1,12 @@
 
 coins = [
-    {'weight': 2.5, 'diameter': .75, 'thickness': 1.52, 'value_in_cents': 1, 'valid': False}
+    {'weight': 2.5, 'diameter': .75, 'thickness': 1.52, 'value_in_cents': 1, 'valid': False},
+    {'weight': 5.0, 'diameter': .835, 'thickness': 1.95, 'value_in_cents': 5, 'valid': True}
 ]
 
 def identify_coin(weight, diameter, thickness):
-    return coins[0]
+    return next((c for c in coins if _compare_dimensions(c, weight, diameter, thickness)), None)
+
+
+def _compare_dimensions(coin, weight, diameter, thickness):
+    return coin['weight'] == weight and coin['diameter'] == diameter and coin['thickness'] == thickness
--- unit_tests/test_money.py
+++ unit_tests/test_money.py
@@ -7,3 +7,8 @@ class TestMoney(unittest.TestCase):
         coin = identify_coin(2.5, .75, 1.52)
 
         assert coin['value_in_cents'] == 1
+
+    def test_identifies_a_nickel(self):
+        coin = identify_coin(5.0, .835, 1.95)
+
+        assert coin['value_in_cents'] == 5
