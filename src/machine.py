e974ab2626a3bd33d18b04a24db3be07fd84ccad
rdmagm062699 <rdmagm062699@gmail.com>
2021-08-22 20:54:59 -0500

    Machine will dispense cola

--- src/machine.py
+++ src/machine.py
@@ -4,3 +4,7 @@ class Machine:
     def __init__(self):
         self.products = []
         self.products.append(Product('COLA', 10))
+
+    def dispense(self, product_name):
+        product = list(filter(lambda p: p.name == product_name.upper(), self.products))[0]
+        product.dispense()
--- unit_tests/test_machine.py
+++ unit_tests/test_machine.py
@@ -8,3 +8,11 @@ class TestMachine(unittest.TestCase):
 
         cola = list(filter(lambda p: p.name == 'COLA', machine.products))[0]
         assert cola.count == 10
+
+    def test_machine_will_dispense_cola(self):
+        machine = Machine()
+
+        machine.dispense('cola')
+        cola = list(filter(lambda p: p.name == 'COLA', machine.products))[0]
+
+        assert cola.count == 9
