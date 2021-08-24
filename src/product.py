88bdc9bc8efd3698cb16e6b9ec459cc74ced76e1
rdmagm062699 <rdmagm062699@gmail.com>
2021-08-23 20:20:01 -0500

    Add price to product

--- src/product.py
+++ src/product.py
@@ -1,7 +1,8 @@
 class Product:
-    def __init__(self, name, count):
+    def __init__(self, name, count, price_in_cents=0):
         self.name = name
         self.count = count
+        self.price_in_cents = price_in_cents
 
     def dispense(self):
         if self.count > 0:
--- unit_tests/test_product.py
+++ unit_tests/test_product.py
@@ -4,13 +4,13 @@ from src.product import Product
 class TestMachine(unittest.TestCase):
 
     def test_dispense_reduces_count_by_one(self):
-        product = Product('COLA', 2)
+        product = Product('COLA', 2, 1.00)
 
         product.dispense()
         assert product.count == 1
 
     def test_product_count_will_never_go_below_0(self):
-        product = Product('COLA', 0)
+        product = Product('COLA', 0, 1.00)
         
         product.dispense()
         assert product.count == 0
