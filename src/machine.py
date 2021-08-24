fabcc73e9ca0a5d63b99154f586728f0d72b3989
rdmagm062699 <rdmagm062699@gmail.com>
2021-08-23 20:54:53 -0500

    Refactor machine dispense method

--- src/machine.py
+++ src/machine.py
@@ -21,18 +21,10 @@ class Machine:
     def dispense(self, product_name):
         product = list(filter(lambda p: p.name == product_name.upper(), self.products))[0]
         
-        if product.price_in_cents > self.amount_in_cents:
-            display_price = format(product.price_in_cents / 100, '.2f')
-            self.display = f'PRICE {display_price}'
-            self.dispenser = ''
-        elif product.count > 0:
-            product.dispense()
-            self.display = 'THANK YOU'
-            self.dispenser = f'DISPENSING {product.name}...'
-            self.amount_in_cents = 0
+        if self._enough_money_entered(product.price_in_cents):
+            self._try_to_dispense(product)
         else:
-            self.dispenser = ''
-            self.display = f'SORRY, WE ARE OUT OF {product.name}'
+            self._display_not_enough_money(product)
 
     def add_coin(self, weight, diameter, thickness):
         coin = identify_coin(weight, diameter, thickness)
@@ -41,3 +33,27 @@ class Machine:
             self.amount_in_cents += coin['value_in_cents']
         else:
             self.coin_return_in_cents += coin['value_in_cents']
+
+    def _enough_money_entered(self, price):
+        return price <= self.amount_in_cents
+
+    def _try_to_dispense(self, product):
+        if product.count > 0:
+            product.dispense()
+            self.amount_in_cents = 0
+            self._display_successful_dispense(product)
+        else:
+            self._display_out_of_product(product)
+
+    def _display_not_enough_money(self, product):
+        display_price = format(product.price_in_cents / 100, '.2f')
+        self.display = f'PRICE {display_price}'
+        self.dispenser = ''
+
+    def _display_successful_dispense(self, product):
+        self.display = 'THANK YOU'
+        self.dispenser = f'DISPENSING {product.name}...'
+
+    def _display_out_of_product(self, product):
+        self.dispenser = ''
+        self.display = f'SORRY, WE ARE OUT OF {product.name}'
